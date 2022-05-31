872dff5b321d88d6bba18ca2092ec16a5e2b7f7f
simbaesther <simbaesther@gmail.com>
2022-05-31 12:39:24 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for j in range(97, 123):
-    if j == 101 or j == 113:
-        continue
-print("{:c}".format(j), end='')
+    if chr(j) != 'q' and chr(j) != 'e':
+        print('{:c}'.format(j), end='')
