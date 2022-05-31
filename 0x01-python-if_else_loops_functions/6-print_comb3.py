c4ce31b324de2e5ebc990719935be9cddf77a7f7
simbaesther <simbaesther@gmail.com>
2022-05-31 13:53:02 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 10):
     for number2 in range(0, 10):
-        if number is 8 and number2 is 9:
+        if number == 8 and number2 == 9:
             print("{:d}{:d}".format(number, number2))
-        elif num < number2:
+        elif number < number2:
             print("{:d}{:d}".format(number, number2), end=", ")
