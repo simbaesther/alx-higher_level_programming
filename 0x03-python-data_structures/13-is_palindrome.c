943548a5e2f171ec897eec00caf99b38b0f1e2fa
Esther Simba <simbaesther@gmail.com>
2022-06-11 11:10:43 +0300

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,14 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	listint_t *head;
+#include <stdio.h>
+void reverse(listint_t **head);
+int compare_lists(listint_t *head, listint_t *middle, int len);
 
-	return (0);
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: pointer to pointer of the first node in listint_t list.
+ * Return: 0 if not a palindrome, 1 if it is a palindrome.
+ */
+int is_palindrome(listint_t **head)
+{
+	int len;
+	int i;
+	listint_t *tmp;
+	listint_t *middle;
+
+	if (head == NULL || *head == NULL)
+		return (1);
+	tmp = *head;
+	middle = *head;
+
+	for (len = 0; tmp != NULL; len++)
+		tmp = tmp->next;
+
+	len = len / 2;
+
+	for (i = 1; i < len; i++)
+		middle = middle->next;
+	if (len % 2 != 0 && len != 1)
+	{
+		middle = middle->next;
+		len = len -1;
+	}
+	reverse(&middle);
+	i = compare_lists(*head, middle, len);
+
+	return (i);
+}
+
+/**
+ * compare_lists - Compares two lists.
+ * @head: pointer to the head node
+ * @middle: Pointer to the middle node.
+ * @len: Length of the lists.
+ * Return: if the lists are the same 1, otherwise 0.
+ */
+int compare_lists(listint_t *head, listint_t *middle, int len)
+{
+	int i;
+
+	if (head == NULL || middle == NULL)
+		return (1);
+	for (i = 0; i < len; i++)
+	{
+		if (head->n != middle->n)
+			return (0);
+		head = head->next;
+		middle = middle->next;
+	}
+	return (1);
+}
+
+/**
+ * reverse - Reverrses a linked list.
+ * @head: pointer to the pointer of the head of a linked list.
+ */
+void reverse(listint_t **head)
+{
+	listint_t *current;
+	listint_t *next;
+	listint_t *prev;
+
+	if (head == NULL || *head == NULL)
+		return;
+	prev = NULL;
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
 }
