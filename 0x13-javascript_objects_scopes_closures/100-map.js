0b8108a561edc85222a93d6fadcd959980499e80
Esther Simba <simbaesther@gmail.com>
2022-09-06 21:17:58 +0300

    changes

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const newList = list.map((x, index) => x * index);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
